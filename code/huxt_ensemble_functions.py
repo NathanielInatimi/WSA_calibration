e899a6afd6ec817f34d2e010f99b2990c5777cba
Nathaniel Inatimi <n.o.edward-inatimi@pgr.reading.ac.uk>
2024-07-15 10:56:18 +0100

    Ensemble generation and statistics, implemented WSA deacceleration

--- code/huxt_ensemble_functions.py
+++ code/huxt_ensemble_functions.py
@@ -246,4 +246,246 @@ def generate_ensemble_forecast(params):
 
     return ensemble_members
 
+def save_ens_to_cdf(ensemble_members, date_string, sigma_latitude, ensemble_size, coronal_model):
+    """
+    saves an ensemble set to a netcdf4 file.
+    saved files indexed by ensemble size, forecast start time, sigma_latitude, and the coronal model used to generate the ensemble
+
+    Args:
+        ensemble_members (list) : list of dataFrames of model runs, timeseries extracted at Earth
+        date_string (string) : date string generated from wsa filename
+        sigma_latitude (float) : scale paramater which controls spread of intial conditions in degrees
+        ensemble_size (int) : number of ensemble members
+        coronal_model (string) : name of coronal model used to generate ensnemble
+        
+    Returns:
+        None
+
+    """
+
+    # Convert list of panda dataframes into single xarray dataset along new index 'ensemble member'
+    ds_ = xr.concat([df.to_xarray() for df in ensemble_members], dim="ensemble_members")
     
+    # Create/find folder indexed by sigma_latitude, ensemble_size, and coronal_model
+    dir_name = f'ens_{int(sigma_latitude)}_{ensemble_size}_{coronal_model}'
+
+    # create directory to save ensemble to
+    directory_path = os.path.abspath(os.path.join(os.pardir,'data','ensembles'))
+    dir_path = os.path.join(directory_path, dir_name)
+    
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path) 
+
+
+    date_num = re.sub('[-/_]','',date_string)
+    # Generate filename for ensemble set (marked by carrington rotation)
+    ensemble_fname = f'ens_{int(sigma_latitude)}_{ensemble_size}_{coronal_model}_{date_num}.nc'
+
+    output_file = os.path.join(directory_path, dir_name, ensemble_fname)
+    
+    # Save the dataset to a NetCDF file
+    ds_.to_netcdf(output_file, engine = 'netcdf4')
+
+    return
+
+def read_ens_cdf(date_string, sigma_latitude, ensemble_size, coronal_model):
+    """
+    reads in netcdf4 file containing an ensemble
+    saved files indexed by ensemble size, forecast start time, sigma_latitude, and the coronal model used to generate the ensemble
+
+    Args:
+        date_string (string) : date string generated from wsa filename
+        sigma_latitude (float) : scale paramater which controls spread of intial conditions in degrees
+        ensemble_size (int) : number of ensemble members
+        coronal_model (string) : name of coronal model used to generate ensnemble
+        
+    Returns:
+        df_list (list) : list of dataFrames of model runs, timeseries extracted at Earth
+
+    """
+    date_num = re.sub('[-/_]','',date_string)
+    
+    # Create/find folder indexed by sigma_latitude, ensemble_size, and coronal_model
+    ens_dir_name = f'ens_{int(sigma_latitude)}_{ensemble_size}_{coronal_model}'
+    ensemble_fname = f'ens_{int(sigma_latitude)}_{ensemble_size}_{coronal_model}_{date_num}.nc'
+    #input_file = f'C:\\Users\\ct832900\\Desktop\\Research_Code\\WSA_calibration\\data\\ensembles\\{dir_name}\\{ensemble_fname}'
+
+    # filepath to read file from
+    data_dir_path = os.path.abspath(os.path.join(os.pardir,'data','ensembles'))
+    ens_dir_path = os.path.join(data_dir_path, ens_dir_name)
+    input_file = os.path.join(ens_dir_path, ensemble_fname)
+
+    # Read the NetCDF file into an xarray Dataset
+    loaded_ds = xr.open_dataset(input_file)
+
+    # Initialize an empty list to store DataFrames
+    df_list = []
+
+    # Loop through ensemble members xarray dimension, convert each member to pandas dataframe, append to list
+    for i in range(ensemble_size):
+        df_list.append(loaded_ds.sel(ensemble_members = i).to_dataframe())
+
+    return df_list
+
+
+def interpolate_and_resample(observed_data, forecast_series):
+    
+    """
+    interpolates forecast timeseries and resamples it onto the observed data timestep
+
+    Args:
+        observed_data (dataFrame) : observed near-Earth solar wind speed data
+        forecast_series (dataFrame) : forecast time series
+        
+    Returns:
+        interpolated_forecast_output (array) : resampled forecast time series onto observed data time step
+
+    """
+    Int = scipy.interpolate.CubicSpline(forecast_series.index, forecast_series['vsw'])
+    
+    data_time_axis = observed_data.index
+
+    interpolated_forecast_output = Int(data_time_axis)
+
+    return interpolated_forecast_output
+
+
+def gen_ranked_ensemble(ensemble_members, observed_data): 
+
+    """
+    ranks an ensemble forecast (counts fraction of ensemble members overpredicting observed wind speed)
+
+    Args:
+        ensemble_members (list) : list of dataFrames of model runs, timeseries extracted at Earth
+        observed_data (dataFrame) : observed near-Earth solar wind speed data
+    
+    Returns:
+        summed_rank (array) : array with counts of ensemble members overprediscting windspeed per timestep
+
+    """
+    vsw_list = [] # Initialise list for forecast output
+
+    # Prepare data for rank comparison
+    data_chunk = observed_data.loc[ensemble_members[0].index[0]:ensemble_members[0].index[-1]]
+    data_chunk = data_chunk.dropna(subset = ['V']) # Remove rows with NaN values
+
+    # Interpolate and resample forecast output onto OMNI data time ste[]
+    for vsw in ensemble_members:
+        vsw_int = interpolate_and_resample(data_chunk, vsw)
+        vsw_list.append(vsw_int)
+
+    # Compare ensemble member output arrays to omni data 
+    vsw_arr = np.array(vsw_list)
+    ranked_forecast_boolean = np.array([vsw < data_chunk['V'] for vsw in vsw_arr])
+    summed_ranks = np.sum(ranked_forecast_boolean, axis = 0)
+
+    return summed_ranks
+
+    
+
+def perturb_longitude(longitudinal_disp, ensemble_member):
+
+    """
+    perturbs an ensemble member longitudinally
+    longitududinal perturbation is equivalent to a temporal displacement of ensemble member
+
+    Args:
+        longitudinal_disp (float) : angular displacement for longitudinal perturbation in units of radians
+        ensemble_member (dataFrame) : single ensemble member dataframe
+    
+    Returns:
+        perturbed_ensemble_member (dataFrame) : ensemble member shifted in time related by the longitudinal displacement
+
+    """
+
+    # convert angular displacement into a temporal displacement
+    carrington_rotation = 27.2753*u.day
+    long_pert_dt = ((longitudinal_disp * carrington_rotation.value) / (2*np.pi)) * u.day
+
+    # Ensure that the DataFrame has a datetime index
+    if not isinstance(ensemble_member.index, pd.DatetimeIndex):
+        raise ValueError("DataFrame must have a datetime index")
+    
+    shift_amount = int(long_pert_dt.to(u.second).value) # Convert shift amount into seconds
+
+    # Convert datetime index into integers
+    numeric_index = ensemble_member.index.astype('int64') // 10**9 # Floor division to convert into seconds (UNIX int datetime is in nanoseconds)
+    numeric_index = numeric_index.to_numpy()
+
+    # Calculate the new index by shifting with wrapping
+    shifted_index = numeric_index + shift_amount
+
+    # Generate boolean array which captures elements above and below max/min indices
+    wrap_mask = (shifted_index < numeric_index.min()) | (shifted_index > numeric_index.max())
+  
+    if shift_amount < 0:
+        # If negative shift amount (i.e. elements will be below min index)
+        shifted_index[wrap_mask] = shifted_index[wrap_mask] + (shifted_index.max() - shifted_index.min())
+
+    elif shift_amount > 0:
+        # If positive shift amount (i.e. elements will be above max index)  
+        shifted_index[wrap_mask] = shifted_index[wrap_mask] - (shifted_index.max() - shifted_index.min())
+
+    else:
+        # No change -- preserve original index
+        shifted_index = numeric_index
+
+    # Convert back to datetime index
+    new_converted_index = pd.to_datetime(shifted_index, unit='s')
+
+    # Sort the DataFrame based on the new index
+    df_shifted = ensemble_member.copy()
+    df_shifted = (df_shifted.sort_index().set_index(new_converted_index)).sort_index()
+    df_shifted = df_shifted[~(df_shifted.index).duplicated()].copy()
+
+    # Interpolate shifted dataframe back onto orignal datetime axis (for more ease in later analysis)
+    Int = scipy.interpolate.CubicSpline(df_shifted.index, df_shifted['vsw'])
+    data_time_axis = ensemble_member.index
+    interpolated_forecast_output = Int(data_time_axis)
+    
+    return pd.DataFrame({'vsw':interpolated_forecast_output}, index = data_time_axis)
+
+def generate_ensemble_statistics(ensemble_members):
+    """
+    computes ensemble statistics which characterise the ensemble spread per timestep. 
+     - range (min/max)
+     - ensemble median/mean
+     - 1 sigma spread (upper/lower)
+     - 2 sigma spread (upper/lower)
+
+    Args:
+        ensemble_members (list) : list of dataFrames timeseries from HUXt model runs
+    
+    Returns:
+        statistics_df (dataFrame) : timeseries of relevant ensemble statistics in each column
+    """
+    
+    # Concatenate dataframes
+    combined_df = pd.concat(ensemble_members, axis=0)
+
+    # Group by time 
+    grouped_df = combined_df.groupby('datetime')['vsw']
+
+    # Compute statistics
+    min_values = grouped_df.min()
+    max_values = grouped_df.max()
+    median_values = grouped_df.median()
+    mean_values = grouped_df.mean()
+    lower_2s_percentile = grouped_df.quantile(0.14)
+    upper_2s_percentile = grouped_df.quantile(0.86)
+    lower_1s_percentile = grouped_df.quantile(0.34)
+    upper_1s_percentile = grouped_df.quantile(0.68)
+
+    # Combine statistics into a single dataframe
+    statistics_df = pd.DataFrame({
+        'min': min_values,
+        'max': max_values,
+        'median' : median_values,
+        'mean' : mean_values,
+        '1s_lower_percentile': lower_1s_percentile,
+        '1s_upper_percentile': upper_1s_percentile,
+        '2s_lower_percentile': lower_2s_percentile,
+        '2s_upper_percentile': upper_2s_percentile
+    })
+
+    return statistics_df
